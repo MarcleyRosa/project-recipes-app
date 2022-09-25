4503d6a46b4b81e1d548d5bbf930bef341cc8a90
Ana <anactc2208@gmail.com>
2022-09-25 17:00:30 -0300

    Requisito 9 pronto, faltando os testes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ headers, isRoute }) {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const showOrHide = () => setShowSearch((prevState) => !prevState);
+
   return (
     <div>
       <h1 data-testid="page-title">{ headers }</h1>
       <Link to="/profile">
         <img data-testid="profile-top-btn" src={ profileIcon } alt="" />
       </Link>
-      <Link to="/search">
+      <button type="button" onClick={ showOrHide }>
         { isRoute && <img data-testid="search-top-btn" src={ searchIcon } alt="" /> }
-      </Link>
+        { showSearch && <input type="text" data-testid="search-input" />}
+      </button>
     </div>
   );
 }
