595bc07b79cb53d72efd92fc73358af2ec58b9f7
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-09-25 15:53:06 -0300

    Alguns testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import Login from '../pages/Login';
 
 describe('Testa a aplicação', () => {
   test('Tela de login', () => {
