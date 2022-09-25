aab917f1f5af1d039239d26f1305a26fad9f0933
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-09-25 15:51:42 -0300

    Alguns testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import Login from '../pages/Login';
 
 describe('Testa a aplicação', () => {
   test('Tela de login', () => {
@@ -13,5 +14,24 @@ describe('Testa a aplicação', () => {
     userEvent.type(inputEmailElement, 'luisfernandesneto@gmail.com');
     userEvent.type(inputPasswordElement, 'xablaus');
     userEvent.click(buttonElement);
+
+    const pageElement = screen.getByTestId('page-title');
+    expect(pageElement).toBeInTheDocument();
+
+    const profileButton = screen.getByTestId('profile-top-btn');
+    const searchButton = screen.getByTestId('search-top-btn');
+
+    expect(profileButton).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+
+    userEvent.click(profileButton);
+
+    const profilePageTitle = screen.getByTestId('page-title');
+    expect(profilePageTitle).toBeInTheDocument();
   });
+  /*  test('Tela Search', () => {
+    render(<App />);
+    const inputEmailElement = screen.getByTestId('email-input');
+    expect(inputEmailElement).toBeInTheDocument();
+  }); */
 });
