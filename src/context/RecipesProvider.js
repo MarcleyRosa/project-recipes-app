d2f0b93d4f2b31e823c73d8e52b908ad0aa80f2e
Marcley <marcleyrosa@gmail.com>
2022-09-26 17:03:27 -0300

    requisito 11

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -6,13 +6,22 @@ function RecipesProvider({ children }) {
   const [state, setState] = useState('');
   const [inputPassword, setInputPassword] = useState('');
   const [inputEmail, setInputEmail] = useState('');
+  const [searchInput, setSearchInput] = useState('a');
+  const [urlSelect, setUrlSelect] = useState('');
+
+  console.log(searchInput);
 
   const contextType = { state,
     setState,
     inputPassword,
     setInputPassword,
     setInputEmail,
-    inputEmail };
+    inputEmail,
+    searchInput,
+    setSearchInput,
+    urlSelect,
+    setUrlSelect,
+  };
 
   return (
     <RecipesContext.Provider value={ contextType }>
