e6cfb5ffc04e7b68216fc7edd4ab7512f5df6536
Marcley <marcleyrosa@gmail.com>
2022-09-30 14:40:05 -0300

    Requisito 25, 26, 27 incompleto, 28, 29, 30, 31 incompleto e 32

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Buttons() {
+  return (
+    <div>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favoritar</button>
+    </div>
+  );
+}
+
+export default Buttons;
